c6e42a5dc9e06c70a2f6a92a45fd5ac257310dcc
Batuhan Türkoğlu <76158371+batuhantrkgl@users.noreply.github.com>
2022-09-04 22:05:59 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_j7xelte.mk
+    $(LOCAL_DIR)/arrow_j7xelte.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_j7xelte-user \
-    lineage_j7xelte-userdebug \
-    lineage_j7xelte-eng
+    arrow_j7xelte-user \
+    arrow_j7xelte-userdebug \
+    arrow_j7xelte-eng
